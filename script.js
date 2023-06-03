b20b8404d1c5eaf503cc5069efc4858c9f928c2d
matty.dev <spulermat@gmail.com>
2023-06-04 00:15:52 +0200

    add fixed nav bar, fix nav padding

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+window.addEventListener("scroll", e => {
+   if (window.scrollY > 50) {
+      document.querySelector("nav").style.backgroundColor = "rgba(255, 255, 255, 0.322)";
+   } else {
+      document.querySelector("nav").style.backgroundColor = "transparent";
+   }
+});
+
 const hamburger = document.querySelector(".nav-hamburger");
 const navToggle = document.querySelector(".nav-toggle");
 
