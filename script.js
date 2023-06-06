4d0d0189075a3bd43b9b8644a992e22055783998
matty.dev <spulermat@gmail.com>
2023-06-06 16:41:03 +0200

    fix gallery nav bar position, add video overlay

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 window.addEventListener("scroll", e => {
    if (window.scrollY > 50) {
       document.querySelector("nav").style.backgroundColor = "rgba(128, 128, 128, 0.300)";
-      document.querySelector("nav").style.backdropFilter = "blur(5px)";
+      document.querySelector("nav").style.backdropFilter = "blur(8px)";
    } else {
       document.querySelector("nav").style.backgroundColor = "transparent";
       document.querySelector("nav").style.backdropFilter = "none";
    }
 });
+
+
 
 const hamburger = document.querySelector(".nav-hamburger");
 const navToggle = document.querySelector(".nav-toggle");
@@ -15,3 +17,10 @@ hamburger.addEventListener("click", e => {
    hamburger.classList.toggle("toggle");
    navToggle.classList.toggle("open");
 });
+
+
+const videoControls = document.querySelector("video");
+
+videoControls.addEventListener("mouseover", function () { this.controls = true; }, false);
+
+videoControls.addEventListener("mouseout", function() { this.controls = false; }, false);
